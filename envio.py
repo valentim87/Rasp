b1e828366e4b36bb3e1607874870b2392a99aa3d
valentim87 <valentim87@hotmail.com>
2015-07-04 02:21:40 -0300

    ok

--- envio.py
+++ envio.py
@@ -5,6 +5,6 @@ ModuloCom.conf("configuracao.txt")
 ModuloCom.start()
 
 
-arquivos = open('/root/projeto/putamerda.txt','r+')
+arquivos = open('/root/projeto/lista.txt','r+')
 for linha in arquivos:
 	ModuloCom.sendArquivo(linha)
